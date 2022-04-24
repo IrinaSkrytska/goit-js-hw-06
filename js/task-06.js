b782f3125769541c3d277162b4695cf26a9ab9d6
Iryna <93982819+IrinaSkrytska@users.noreply.github.com>
2022-04-24 13:11:42 +0300

    updates 6 task

--- js/task-06.js
+++ js/task-06.js
@@ -4,7 +4,7 @@ const dataLength = inputText.getAttribute("data-length");
 inputText.addEventListener("blur", onInputBlur);
 
 function onInputBlur(event) {
-  const inputDatalength = event.currentTarget.value.length >= Number(dataLength);
+  const inputDatalength = event.currentTarget.value.length === Number(dataLength);
  
   inputDatalength
     ? inputText.classList.add("valid") & inputText.classList.remove("invalid")
